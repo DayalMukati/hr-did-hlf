d10205bd6631635ce59f24fdf1ad26942e706a52
Dayal Mukati <dayalmukati@gmail.com>
2025-03-07 11:35:29 +0530

    chaincode update

--- chaincode/did.go
+++ chaincode/did.go
@@ -22,112 +22,25 @@ type DID struct {
 
 // CreateDID creates a new decentralized identity
 func (s *SmartContract) CreateDID(ctx contractapi.TransactionContextInterface, did string, name string, credentials string) error {
-	existingDID, err := ctx.GetStub().GetState(did)
-	if err != nil {
-		return fmt.Errorf("failed to read from world state: %v", err)
-	}
-	if len(existingDID) > 0 {
-		return fmt.Errorf("DID already exists")
-	}
-
-	identity := DID{
-		DID:         did,
-		Name:        name,
-		Credentials: credentials,
-		Verified:    false,
-	}
-
-	identityJSON, err := json.Marshal(identity)
-	if err != nil {
-		return err
-	}
-
-	return ctx.GetStub().PutState(did, identityJSON)
+	
 }
 
 // UpdateCredentials updates the credentials of an existing DID
 func (s *SmartContract) UpdateCredentials(ctx contractapi.TransactionContextInterface, did string, newCredentials string) error {
-	didJSON, err := ctx.GetStub().GetState(did)
-	if err != nil {
-		return fmt.Errorf("failed to read DID: %v", err)
-	}
-	if len(didJSON) == 0 {
-		return fmt.Errorf("DID does not exist")
-	}
-
-	var identity DID
-	err = json.Unmarshal(didJSON, &identity)
-	if err != nil {
-		return err
-	}
-
-	identity.Credentials = newCredentials
-
-	updatedDIDJSON, err := json.Marshal(identity)
-	if err != nil {
-		return err
-	}
-
-	return ctx.GetStub().PutState(did, updatedDIDJSON)
+	
 }
 
 // VerifyDID marks a DID as verified
 func (s *SmartContract) VerifyDID(ctx contractapi.TransactionContextInterface, did string) error {
-	didJSON, err := ctx.GetStub().GetState(did)
-	if err != nil {
-		return fmt.Errorf("failed to read DID: %v", err)
-	}
-	if len(didJSON) == 0 {
-		return fmt.Errorf("DID does not exist")
-	}
-
-	var identity DID
-	err = json.Unmarshal(didJSON, &identity)
-	if err != nil {
-		return err
-	}
-
-	identity.Verified = true
-
-	updatedDIDJSON, err := json.Marshal(identity)
-	if err != nil {
-		return err
-	}
-
-	return ctx.GetStub().PutState(did, updatedDIDJSON)
+	
 }
 
-// RevokeDID removes a DID from the ledger
-func (s *SmartContract) RevokeDID(ctx contractapi.TransactionContextInterface, did string) error {
-	existingDID, err := ctx.GetStub().GetState(did)
-	if err != nil {
-		return fmt.Errorf("failed to read DID: %v", err)
-	}
-	if len(existingDID) == 0 {
-		return fmt.Errorf("DID does not exist")
-	}
 
-	// Remove the DID from state
-	return ctx.GetStub().DelState(did)
-}
+
 
 // GetDID retrieves the details of a DID
 func (s *SmartContract) GetDID(ctx contractapi.TransactionContextInterface, did string) (*DID, error) {
-	didJSON, err := ctx.GetStub().GetState(did)
-	if err != nil {
-		return nil, fmt.Errorf("failed to read DID: %v", err)
-	}
-	if len(didJSON) == 0 {
-		return nil, fmt.Errorf("DID does not exist")
-	}
-
-	var identity DID
-	err = json.Unmarshal(didJSON, &identity)
-	if err != nil {
-		return nil, err
-	}
-
-	return &identity, nil
+	
 }
 
 // Main function to start the chaincode
